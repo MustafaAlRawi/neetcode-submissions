543647732f0436d90e77264e2b56a874c79cce07
MustafaAlRawi <MustafaAlRawi@users.noreply.github.com>
2026-08-01 11:46:21 -0400

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func minCostClimbingStairs(_ cost: [Int]) -> Int {
+        guard cost.count > 1 else {
+            return cost.first ?? 0
+        }
+        var dp = Array(repeating: 0, count: cost.count)
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        
+        for i in 2..<cost.count {
+            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i]
+        }
+        return min(dp[cost.count - 1], dp[cost.count - 2])
+    }
+}
